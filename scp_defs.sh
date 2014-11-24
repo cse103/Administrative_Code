73e5977884a7825acf3e8ff5ae1f7efc08b60072
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-11-23 18:47:34 -0800

    Update script

--- scp_defs.sh
+++ scp_defs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python CSV_to_defs.py
-scp setWeek*.def webwork.cse.ucsd.edu:/opt/webwork/courses/CSE103_Fall14/templates/
+scp set*.def webwork.cse.ucsd.edu:/opt/webwork/courses/CSE103_Fall14/templates/
