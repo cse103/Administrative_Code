5a41ac37edb914e3a3cdcb49c816e59efb198ac6
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-12-01 15:20:55 -0800

    Add review problems, also generate a problem set with unselected problems.

--- CSV_to_defs.py
+++ CSV_to_defs.py
@@ -3,13 +3,28 @@
 import pandas as pd
 import argparse
 import datetime
+from datetime import timedelta
 
 df = pd.read_csv('set_assignments.csv')
 df
 
 sets = sorted(set(df['Set Name']))
-start_date = datetime.datetime(2014, 10, 1, 12)
+print set(df['Set Name'])
+start_date = datetime.datetime(2014, 10, 1, 17)
 week = datetime.timedelta(days=7)
+
+unused_problems = open('setUnusedProblems.def', 'w')
+unused_problems.write("openDate          = {0} PST\n".format(
+    (datetime.datetime.now() + week).strftime('%m/%d/%Y at %I:%M%p')))
+unused_problems.write("dueDate           = {0} PST\n".format(
+    (datetime.datetime.now() + 2*week).strftime('%m/%d/%Y at %I:%M%p')))
+unused_problems.write("answerDate        = {0} PST\n".format(
+    (datetime.datetime.now() + 2*week).strftime('%m/%d/%Y at %I:%M%p')))
+
+unused_problems.write("paperHeaderFile   = defaultHeader\n")
+unused_problems.write("screenHeaderFile  = defaultHeader\n")
+unused_problems.write("problemList       =\n")
+
 for set_id in sets:
     with open("set{0}.def".format(set_id), 'w') as def_file:
         end_date = start_date + week
@@ -27,7 +42,9 @@ for set_id in sets:
         def_file.write("problemList       =\n")
 
         start_date = end_date
-        set_problems = df[df['Set Name']==set_id]
+        set_problems = df[df['Set Name'] == set_id]
         for index, problem in set_problems.iterrows():
             if problem['Used'] == 1:
                 def_file.write("{0}\n".format(problem['PG File Path']))
+            else:
+                unused_problems.write("{0}\n".format(problem['PG File Path']))
